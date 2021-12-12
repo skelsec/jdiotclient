67a36a6f2bc48910c41cb23fa104b7b4710bc2d9
skelsec <info@skelsec.com>
2021-12-13 00:37:16 +0100

    meh

--- setup.py
+++ setup.py
@@ -48,10 +48,10 @@ setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	entry_points={
-		'console_scripts': [
-			'asmbclient = aiosmb.examples.smbclient:main',
-		],
-
-	}
+	#entry_points={
+	#	'console_scripts': [
+	#		'asmbclient = aiosmb.examples.smbclient:main',
+	#	],
+	#
+	#}
 )
